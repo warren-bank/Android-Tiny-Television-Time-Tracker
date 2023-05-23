e0b7d072989c640535f7b13d260319fd832eb841
Warren R. Bank <warren.r.bank@gmail.com>
2023-05-22 22:31:50 -0700

    bug fix: DroidShows database migration
    Remove unused fields from the 'FindResults' class.
    Class is rehydrated by deserialization of JSON from 'find' API response.
    Some nested JSON objects are incomplete, which throws 'MAPPING_FAILED'.
    
    Specifically:
      {"tv_episode_results": [{...fields}]}
    Where the following fields are included:
      "id"
      "name"
      "overview"
      "media_type"
      "vote_average"
      "vote_count"
      "air_date"
      "episode_number"
      "production_code"
      "runtime"
      "season_number"
      "show_id"
      "still_path"
    However, the following fields are missing:
      "backdrop_path"
      "poster_path"
      "popularity"
    
    Specifically:
    * themoviedbapi/model/FindResults.java
        public class FindResults extends AbstractJsonMapping {
          @JsonProperty("tv_episode_results")
          private List<TVEpisodeBasic> tvEpisodeResults;
        }
    * themoviedbapi/model/tv/TVEpisodeBasic.java
        public class TVEpisodeBasic extends MediaBasic {
          @JsonProperty("air_date")
          @JsonProperty("season_number")
          @JsonProperty("episode_number")
          @JsonProperty("name")
          @JsonProperty("overview")
          @JsonProperty("still_path")
          @JsonProperty("show_id")
        }
    * themoviedbapi/model/media/MediaBasic.java
        public class MediaBasic extends AbstractJsonMapping {
          @JsonProperty("id")
          @JsonProperty("backdrop_path")
          @JsonProperty("poster_path")
          @JsonProperty("popularity")
          @JsonProperty("vote_average")
          @JsonProperty("vote_count")
        }

--- android-studio-project/libs/api-themoviedb/src/main/java/com/omertron/themoviedbapi/model/FindResults.java
+++ android-studio-project/libs/api-themoviedb/src/main/java/com/omertron/themoviedbapi/model/FindResults.java
@@ -20,11 +20,7 @@
 package com.omertron.themoviedbapi.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.omertron.themoviedbapi.model.movie.MovieBasic;
-import com.omertron.themoviedbapi.model.person.PersonFind;
 import com.omertron.themoviedbapi.model.tv.TVBasic;
-import com.omertron.themoviedbapi.model.tv.TVEpisodeBasic;
-import com.omertron.themoviedbapi.model.tv.TVSeasonBasic;
 import java.io.Serializable;
 import java.util.List;
 
@@ -35,32 +31,8 @@ public class FindResults extends AbstractJsonMapping implements Serializable {
 
     private static final long serialVersionUID = 100L;
 
-    @JsonProperty("movie_results")
-    private List<MovieBasic> movieResults;
-    @JsonProperty("person_results")
-    private List<PersonFind> personResults;
     @JsonProperty("tv_results")
     private List<TVBasic> tvResults;
-    @JsonProperty("tv_season_results")
-    private List<TVSeasonBasic> tvSeasonResults;
-    @JsonProperty("tv_episode_results")
-    private List<TVEpisodeBasic> tvEpisodeResults;
-
-    public List<MovieBasic> getMovieResults() {
-        return movieResults;
-    }
-
-    public void setMovieResults(List<MovieBasic> movieResults) {
-        this.movieResults = movieResults;
-    }
-
-    public List<PersonFind> getPersonResults() {
-        return personResults;
-    }
-
-    public void setPersonResults(List<PersonFind> personResults) {
-        this.personResults = personResults;
-    }
 
     public List<TVBasic> getTvResults() {
         return tvResults;
@@ -68,22 +40,6 @@ public class FindResults extends AbstractJsonMapping implements Serializable {
 
     public void setTvResults(List<TVBasic> tvResults) {
         this.tvResults = tvResults;
-    }
-
-    public List<TVSeasonBasic> getTvSeasonResults() {
-        return tvSeasonResults;
-    }
-
-    public void setTvSeasonResults(List<TVSeasonBasic> tvSeasonResults) {
-        this.tvSeasonResults = tvSeasonResults;
-    }
-
-    public List<TVEpisodeBasic> getTvEpisodeResults() {
-        return tvEpisodeResults;
-    }
-
-    public void setTvEpisodeResults(List<TVEpisodeBasic> tvEpisodeResults) {
-        this.tvEpisodeResults = tvEpisodeResults;
     }
 
 }
